0f668c8c76b1356a1311f25492e0039b44bbcdfe
hoangphi9819-sys <hoangphi9819@gmail.com>
2026-09-14 13:32:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('models/gemini-1.5-flash')
 
 @app.route("/", methods=['GET'])
 def index():
